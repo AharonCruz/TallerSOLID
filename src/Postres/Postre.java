956dfad465fa4db10a954e997367eb6eddfb0a63
AharonCruz <ahajcruz@espol.edu.ec>
2020-06-25 10:58:29 -0500

    Se creo la clase Postre y ahora la clase pastel y helado son sublcases de esta, se eliminó la clase OperacionesAderezo y ahora la clase Postre tiene sus métodos.

---
+++ src/Postres/Postre.java
@@ -0,0 +1,50 @@
+package Postres;
+
+import java.util.ArrayList;
+
+import Adicionales.Aderezo;
+
+public class Postre {
+	protected String sabor;
+	protected double precioParcial;
+	protected ArrayList<Aderezo> aderezos;
+
+	public Postre(String sabor) {
+		aderezos = new ArrayList<>();
+		this.sabor = sabor;
+	}
+
+	public double calcularPrecioFinal() {
+		double precioFinal;
+		precioFinal = (precioParcial + (precioParcial * 0.12)) + (aderezos.size() * 0.50);
+		return precioFinal;
+	}
+	
+	
+	public  void anadirAderezoHelado(Aderezo aderezo){
+        this.getAderezos().add(aderezo);
+    }
+    
+    public  void quitarAderezoHelado(Aderezo aderezo){
+        this.getAderezos().remove(aderezo);
+    }
+    
+    public  void anadirAderezoPastel(Aderezo aderezo){
+        this.getAderezos().add(aderezo);
+    }
+    
+    public void quitarAderezoPastel(Aderezo aderezo){
+        this.getAderezos().remove(aderezo);
+    }
+
+	public ArrayList<Aderezo> getAderezos() {
+		return aderezos;
+	}
+
+
+	public String showPrecioFinal() {
+		return "Precio Final: $ " + calcularPrecioFinal();
+	}
+	
+	
+}
